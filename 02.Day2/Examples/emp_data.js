e3ce74bfc7442c3d9bca4f3e287e3a9ad57b44bc
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2023-04-19 12:49:51 +0530

    Add files via upload

---
+++ 02.Day2/Examples/emp_data.js
@@ -0,0 +1,22 @@
+
+// Named export 
+/*
+export  const empData =  function() {
+		const name = "James";
+		const job = "Manager";
+		const company  =  "Ness Technologies";
+		return `Mr./Mrs.  ${name} working as a ${job} in ${company}`;
+}; 
+*/
+
+// default export 
+ const empData =  function() {
+		const name = "James";
+		const job = "Manager";
+		const company  =  "Ness Technologies";
+		return `Mr./Mrs.  ${name} working as a ${job} in ${company}`;
+}; 
+
+
+export default empData;
+	
